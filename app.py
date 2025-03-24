b7c2ad7ba2bdd27bfce0000755e31dcc68357153
edhyland <rabbitholeexploration@gmail.com>
2025-03-24 17:20:14 -0400

    Add files via upload
    updated code

--- app.py
+++ app.py
@@ -460,13 +460,15 @@ else:
 st.markdown("---")  # Add a horizontal line for separation
 st.markdown(
     """
-   <form action="https://www.paypal.com/donate" method="post" target="_top">
+<form action="https://www.paypal.com/donate" method="post" target="_top">
 <input type="hidden" name="business" value="HH5K872YT7UBQ" />
 <input type="hidden" name="no_recurring" value="0" />
+<input type="hidden" name="item_name" value="Anything that you contribute will help make this project better." />
 <input type="hidden" name="currency_code" value="USD" />
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 </form>
+
 
     """,
     unsafe_allow_html=True
